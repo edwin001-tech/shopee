b4d673d372f196aeba6f026c040285dd5d31b4c0
edwin001-tech <edwinshitakwa8@gmail.com>
2022-02-13 14:15:14 +0300

    get products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,14 +2,14 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll() 
-  .then(([rows, fieldData]) => {
-    res.render('shop/product-list', {
-      prods: rows,
-      pageTitle: 'All Products',
-      path: '/products'  
-    });
-  })
+  Product.findAll() 
+  .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
   .catch(err => console.log(err));
 };
     
@@ -28,17 +28,19 @@ exports.getProduct = (req, res, next) => {
   })
   .catch(err => console.log(err)); 
 };
-// rows are the entries in the product table
+
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
-    res.render('shop/index', {
-      prods: rows,
-      pageTitle: 'Shop',
-      path: '/'  
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
