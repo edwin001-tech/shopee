d5d9ec29eabac532488f17181a9a92c3fbddad92
edwin001-tech <edwinshitakwa8@gmail.com>
2022-03-31 23:30:51 +0300

    shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,8 +2,8 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll() 
-  .then(products => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
