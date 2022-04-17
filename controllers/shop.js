4898818e1b4283892378a75a644069f6c0bc05c7
edwin001-tech <edwinshitakwa8@gmail.com>
2022-04-17 13:26:21 +0300

    post order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -112,9 +112,37 @@ exports.postCartDeleteProduct = (req, res, next) => {
       return product.cartItem.destroy();
     })
     .then(result => {
-    res.redirect('/cart');
-  })
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        //get created order then add product to the order with the quantity
+        .then(order => {
+          return order.addProduct(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      res.redirect('/orders')
+    })
+    .catch(err => console.log(err));
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
