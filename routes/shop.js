27c223d5e888dc43677acf8134d7f430b2500e17
edwin001-tech <edwinshitakwa8@gmail.com>
2022-04-22 17:26:28 +0300

    get products

--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,10 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-// router.get('/', shopController.getIndex);
-// // let me see if this works
+router.get('/', shopController.getIndex);
+// let me see if this works
 
-// router.get('/products', shopController.getProducts);
+router.get('/products', shopController.getProducts);
 // //dynamic route
 // router.get('/products/:productId', shopController.getProduct);
 
