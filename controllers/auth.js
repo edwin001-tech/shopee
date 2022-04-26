19c0321813d0e763eb5144cc3914fb739b0e516b
edwin001-tech <edwinshitakwa8@gmail.com>
2022-04-27 02:10:03 +0300

    implement a cookie

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get('Cookie')
+    .split(';')[1]
+    .trim()
+    .split('=')[1];
+      res.render('auth/login', {
+        
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
+      });
+};
+
+exports.postLogin = (req, res, next) => {
+  //this header will be sent with every request
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
+}
