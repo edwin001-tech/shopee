ef6b65338c252f135df9416d5962af2dbc742f8e
edwin001-tech <edwinshitakwa8@gmail.com>
2022-04-30 13:56:09 +0300

    csrf Token

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const mongoDBSessionStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,7 +17,9 @@ const app = express();
 const store = new mongoDBSessionStore({
   uri: MONGODB_URI,
   collection: 'sessions'
-})
+});
+
+const csrfProtection = csrf();
 
 
 app.set('view engine', 'ejs');
@@ -39,6 +42,7 @@ app.use(
     store: store
   })
 );
+app.use(csrfProtection);
 
 
 //Dummy user
@@ -53,6 +57,13 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 });
+
+//set local variables that are passed into the views
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
