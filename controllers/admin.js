bfa9d1a7032952e5d22833a9e842067df680ac0a
edwin001-tech <edwinshitakwa8@gmail.com>
2022-05-13 16:44:27 +0300

    authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -66,21 +66,26 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
+      //ensure we can't edit products belonging to us
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
-      return product.save();
-  })
-    .then(result => {
+      return product.save()
+        .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
+  })
+     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  //filter products for the currently logged in user
+  Product.find({userId: req.user._id})
     //control which fields to return
     // .select('title price -_id')
     // .populate('userId', 'name')
@@ -96,7 +101,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
